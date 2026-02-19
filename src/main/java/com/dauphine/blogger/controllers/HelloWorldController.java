7b5e93f3ec3268b97c3dae368c1541fc2fd00bb6
T1toulol <titouan.ropartz@gmail.com>
2026-02-19 15:50:42 +0100

    Partie 2 avec la documentation (pas finie)

--- src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
+++ src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.dauphine.blogger.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,11 @@ public class HelloWorldController {
 
     @GetMapping("hello")
     public String HelloByName(@RequestParam String name){
+        return "Hello" + name;
+    }
+
+    @GetMapping("hello/{name}")
+    public String Hello(@PathVariable String name){
         return "Hello" + name;
     }
 }
