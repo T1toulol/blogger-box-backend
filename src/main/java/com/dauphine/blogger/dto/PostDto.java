5c6a71567922a8195ef4fbed4d6c0f4c919bc176
T1toulol <titouan.ropartz@gmail.com>
2026-03-12 16:19:01 +0100

    format category date in post

--- src/main/java/com/dauphine/blogger/dto/PostDto.java
+++ src/main/java/com/dauphine/blogger/dto/PostDto.java
@@ -1,10 +1,13 @@
 package com.dauphine.blogger.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.Instant;
 import java.util.List;
 
 /**
- * Simple DTO for Post used to expose endpoints (stubbed / not implemented).
+ * Simple DTO for Post used to expose endpoints.
  */
-public record PostDto(Long id, String title, String content, Instant createdDate, List<Long> categoryIds) {
+public record PostDto(Long id, String title, String content,
+					  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "UTC") Instant createdDate,
+					  List<Long> categoryIds) {
 }
